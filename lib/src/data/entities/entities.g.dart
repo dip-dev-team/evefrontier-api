b79c50a0fbb377f2f54947c3624bf3e0b345731a
Dmytro Prylutskyi <dmytro.prylutskyi@outlook.com>
2025-04-10 11:05:04 +0200

    Add generated files

---
+++ lib/src/data/entities/entities.g.dart
@@ -0,0 +1,108 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'entities.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+Serializers _$entititySerializers = (new Serializers().toBuilder()
+      ..add(AbiCfgEntity.serializer)
+      ..add(AbiConfigEntity.serializer)
+      ..add(AbiEntity.serializer)
+      ..add(AbiProportiesEntity.serializer)
+      ..add(AddressConfigEntity.serializer)
+      ..add(AllTypesDataAttributesEntity.serializer)
+      ..add(AllTypesDataEntity.serializer)
+      ..add(AttributeEntity.serializer)
+      ..add(AttributePropertiesEntity.serializer)
+      ..add(ChainConfigEntity.serializer)
+      ..add(CharacterEntity.serializer)
+      ..add(ContractsEntity.serializer)
+      ..add(EipEntity.serializer)
+      ..add(EphemeralInventoryEntity.serializer)
+      ..add(ErcEntity.serializer)
+      ..add(FuelModuleEntity.serializer)
+      ..add(GateLinkModuleEntity.serializer)
+      ..add(HeatlhyEntity.serializer)
+      ..add(InventoryItemEntity.serializer)
+      ..add(InventoryModuleEntity.serializer)
+      ..add(ItemTypeIDsEntity.serializer)
+      ..add(KillMailEntity.serializer)
+      ..add(LocationEntity.serializer)
+      ..add(LossTypeEnum.serializer)
+      ..add(MetadataEntity.serializer)
+      ..add(NativeCurrencyEntity.serializer)
+      ..add(ProximityModuleEntity.serializer)
+      ..add(RpcEntity.serializer)
+      ..add(RpcUrlsEntity.serializer)
+      ..add(SimpleSmartAssemblyEntity.serializer)
+      ..add(SmartAssemblyEntity.serializer)
+      ..add(SmartAssemblyEnum.serializer)
+      ..add(SmartCharacterEntity.serializer)
+      ..add(SolarSystemEntity.serializer)
+      ..add(StaticDataEntity.serializer)
+      ..add(SystemIdsEntity.serializer)
+      ..add(UrlsEntity.serializer)
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(AbiCfgEntity)]),
+          () => new ListBuilder<AbiCfgEntity>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(AbiEntity)]),
+          () => new ListBuilder<AbiEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(AbiProportiesEntity)]),
+          () => new ListBuilder<AbiProportiesEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(AbiProportiesEntity)]),
+          () => new ListBuilder<AbiProportiesEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(AbiProportiesEntity)]),
+          () => new ListBuilder<AbiProportiesEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(AllTypesDataAttributesEntity)]),
+          () => new ListBuilder<AllTypesDataAttributesEntity>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(AttributeEntity)]),
+          () => new ListBuilder<AttributeEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(EphemeralInventoryEntity)]),
+          () => new ListBuilder<EphemeralInventoryEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(InventoryItemEntity)]),
+          () => new ListBuilder<InventoryItemEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(InventoryItemEntity)]),
+          () => new ListBuilder<InventoryItemEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(SimpleSmartAssemblyEntity)]),
+          () => new ListBuilder<SimpleSmartAssemblyEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(SimpleSmartAssemblyEntity)]),
+          () => new ListBuilder<SimpleSmartAssemblyEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(SimpleSmartAssemblyEntity)]),
+          () => new ListBuilder<SimpleSmartAssemblyEntity>())
+      ..addBuilderFactory(
+          const FullType(
+              BuiltList, const [const FullType(SimpleSmartAssemblyEntity)]),
+          () => new ListBuilder<SimpleSmartAssemblyEntity>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(String)]),
+          () => new ListBuilder<String>())
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(String)]),
+          () => new ListBuilder<String>()))
+    .build();
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
